25e49dfc4fd0c9869ee319e5f8f24462af090498
Zabro000 <125840656+Zabro000@users.noreply.github.com>
2025-02-28 23:46:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# General-Template-Repository
+ 
---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    print("HI")
+    print("Hi", end=" ")
+    print("sdasdasd", end="\t")
+    print(":k")
+    print(10 % 3)
+    print(10 // 3)
+    print("Hi")
+
+    
+
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
