60022e0f3abb0b8e2276b644d5089d609deda1f7
Zabro <reinzabrocki@shaw.ca>
2025-07-01 18:38:30 -0600

    Doubled hold up capacitance and shortened turn on time

--- Enable divider bottom res calcs.py
+++ Enable divider bottom res calcs.py
@@ -2,7 +2,7 @@
 
 comparator_voltage = 5
 hysteresis = (200 / 2) * 10 ** (-3)
-triggering_voltage = 15.6
+triggering_voltage = 15.3
 
 top_res = 6.8e4
 def bottom_resistor(input_voltage, top_res, hysteresis, comparator_trigger):
