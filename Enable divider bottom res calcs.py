b305dde9ff3da681e5c7d79b7e63ee9a6ce33c9f
Zabro <reinzabrocki@shaw.ca>
2025-07-01 18:24:33 -0600

    Added 5A current source and changed comparator resistor values

--- Enable divider bottom res calcs.py
+++ Enable divider bottom res calcs.py
@@ -2,7 +2,8 @@
 
 comparator_voltage = 5
 hysteresis = (200 / 2) * 10 ** (-3)
-triggering_voltage = 14
+triggering_voltage = 15.6
+
 top_res = 6.8e4
 def bottom_resistor(input_voltage, top_res, hysteresis, comparator_trigger):
     return ((comparator_trigger - hysteresis)* top_res) / (input_voltage - (comparator_trigger - hysteresis))
