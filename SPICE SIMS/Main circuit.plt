1b62f388b80c7f7605ed56c255fa3abfed27a23d
Zabro <reinzabrocki@shaw.ca>
2025-07-06 22:33:08 -0600

    Power loads are not working, there is a weird oscillation

--- SPICE SIMS/Main circuit.plt
+++ SPICE SIMS/Main circuit.plt
@@ -2,10 +2,10 @@
 {
    Npanes: 1
    {
-      traces: 5 {268959747,0,"V(n005)"} {524290,0,"V(umbilical_priority_comparator)"} {524292,0,"V(umbilical)"} {524293,0,"V(umbilical_turn_on)"} {524294,0,"V(umbilical_gate_control_logic_output)"}
-      X: ('m',0,0,0.09,0.9)
+      traces: 3 {524292,0,"V(n002)"} {524290,0,"V(umbilical_priority_comparator)"} {524291,0,"V(umbilical)"}
+      X: ('m',0,0,0.001,0.0124618138645579)
       Y[0]: (' ',0,-5,5,50)
-      Y[1]: (' ',1,1e+308,0.6,-1e+308)
+      Y[1]: (' ',0,1e+308,1,-1e+308)
       Volts: (' ',0,0,1,-5,5,50)
       Log: 0 0 0
       GridStyle: 1
--- a.py
+++ a.py
@@ -1,3 +1,25 @@
-class LMR38025:
-    def __init__(self):
-        pass
+from sympy import symbols
+from sympy.plotting import plot
+from sympy.integrals import laplace_transform
+
+
+
+"""x, u ,s = symbols('x u s') 
+f = sy.exp(-(x - u) ** 2 / (2 * s ** 2)) / (s * (2 * sy.pi) ** (1/2))
+
+ix = sy.integrate(f,x)
+print(ix)
+r = ix.subs(x, 10).subs(u, 1).subs(s, 10)
+print(float(r))"""
+
+
+from sympy import laplace_transform, diff, Function
+from sympy import laplace_correspondence, laplace_initial_conds
+from sympy.abc import t, s
+y = Function("y")
+Y = Function("Y")
+f = laplace_transform(diff(y(t), t, 3), t, s, noconds=True)
+g = laplace_correspondence(f, {y: Y})
+print(laplace_initial_conds(g, t, {y: [2, 4, 8, 16, 32]}))
+
+
