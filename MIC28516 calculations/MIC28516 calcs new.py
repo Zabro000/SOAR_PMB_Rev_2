f1d3c202bc3fa9c7a3842811626894b0c987bf6f
Zabro000 <reinzabrocki@shaw.ca>
2025-08-25 16:55:36 -0600

    Added plots but kept the normal buck_12V function

--- MIC28516 calculations/MIC28516 calcs new.py
+++ MIC28516 calculations/MIC28516 calcs new.py
@@ -40,11 +40,44 @@ def buck_12V():
         buck_1.ripple_injection_calculations_given_components_known()
         listt = np.append(listt, buck_1.peak_to_peak_feedback_voltage_ripple_using_method_3)
 
-    fig, ax = plt.subplots(figsize = (4,3))
-    ax.plot(ripple_res, listt, color = 'black', linestyle = 'dashed', marker = 'o')
+    fig, ax = plt.subplots(figsize = (4*2,3*2))
+    ax.plot(ripple_res, listt, color = 'green', linestyle = 'dashed', marker = 'o')
     ax.grid(True, color = 'k', linestyle = "--")
+    ax.set_xlabel("Ripple Injection Resistor Value (ohm)")
+    ax.set_ylabel("Injected Ripple (V)")
+    ax.set_title("Injected Ripple vs Ripple Injection Resistor Value")
 
     plt.show()
+
+
+def buck_12V_1():
+    # defining inital values
+    fsw = 300e3
+    vin =  48
+    vout = 12 
+    tss = 30e-3
+    ripple_ratio = 0.2
+    fb_rtop = 21e3
+    curent_limit = 8
+    c_out = 330e-6
+    esr_c_out = 14e-3
+    feedforward_cap = 4.7e-9
+    ripple_resistor = 39e3
+    ripple_capacitor = 100e-9
+    eff = 0.9
+    winding_res = 1.9e-3
+
+    buck_1 = Buck.MIC28516(fsw, vin, vout, tss, ripple_ratio, fb_rtop, curent_limit, output_capacitance = c_out, output_capacitance_esr = esr_c_out, 
+                      ripple_injection_resistor= ripple_resistor, ripple_injection_capacitor = ripple_capacitor, feedforward_capacitor = feedforward_cap,
+                      typical_efficiency = eff, inductor_winding_resistance = winding_res)
+    
+    buck_1.run_all_calcs_compare()
+
+    buck_1.input_voltage = 16.8
+    set_ind = buck_1.inductance
+
+    buck_1.run_all_calcs_compare(set_inductance = True, set_inductance_value = set_ind)
+
 
 
 def buck_5V():
@@ -99,6 +132,7 @@ def buck_3V3():
 
 def main():
     buck_12V()
+    buck_12V_1()
 
 
 if __name__ == "__main__":
