2daaf7dd12dd7d4e829aef988e8e07dc91802c15
Zabro000 <reinzabrocki@shaw.ca>
2025-08-30 23:26:35 -0600

    Working on the PCB lib for general PCB calculations

---
+++ Board and Component Calculations/PCB_lib.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+import math as M 
+from engineering_notation import EngNumber
+
+
+# Make the assumption that there are n many buck converters
+# Assume all buck converters are in parallel 
+class PCB_Object():
+
+    def __init__(self, converter_efficiencies, converter_nominal_output_currents, converter_output_voltages, 
+                 input_voltage: float, current_safety_factor: float = None):
+        
+        self.converter_efficiencies = np.array(converter_efficiencies)
+        self.converter_nominal_output_currents = np.array(converter_nominal_output_currents)
+        self.converter_output_voltages = np.array(converter_output_voltages)
+        self.input_voltage = input_voltage
+
+        self.total_input_power = None
+        self.total_output_power = None
+        self.input_power_per_converter = None
+        self.output_power_per_converter = None
+        self.input_current_per_converter = None
+        self.total_nominal_input_current = None
+        self.total_nominal_input_current_with_safety_factor = None 
+
+        if current_safety_factor is None:
+            self.current_safety_factor = None 
+        else:
+            self.current_safety_factor = current_safety_factor
+
+    def compute_output_powers(self): 
+        self.output_power_per_converter = np.multiply(self.converter_output_voltages, self.converter_nominal_output_currents)
+        self.total_output_power = np.sum(self.output_power_per_converter)
+
+    def compute_input_powers(self):
+        self.input_power_per_converter = np.divide(self.output_power_per_converter, self.converter_efficiencies)
+        self.total_input_power = np.sum(self.input_power_per_converter)
+
+
+    def compute_input_currents(self):
+        self.input_current_per_converter = self.input_power_per_converter * (1 / self.input_voltage)
+        self.total_nominal_input_current = np.sum(self.input_current_per_converter)
+
+    def compute_total_input_current_with_safety_factor(self):
+        self.total_nominal_input_current_with_safety_factor = self.total_nominal_input_current * (1 + self.current_safety_factor)
+
+
+    def test_computation(self):
+        self.compute_output_powers()
+        self.compute_input_powers()
+        self.compute_input_currents()
+        self.compute_total_input_current_with_safety_factor()
+
+        print(self.output_power_per_converter)
+        print(self.input_power_per_converter)
+        print(self.total_input_power)
+        print(self.total_output_power)
+        print(self.input_current_per_converter)
+        print(self.total_nominal_input_current)
+        print(self.total_nominal_input_current_with_safety_factor)
+        
+
+        
+class Power_Trace(PCB_Object):
+    def __init__(self):
+        super().__init__()
+
+
+def test():
+    eff_avg = 0.92
+    eff = [eff_avg, eff_avg, eff_avg]
+    out = [12, 5, 3.3]
+    curr = [8, 8, 8]
+    input_1 = 16.8
+    safety = 0.15
+
+    pmb = PCB_Object(eff, curr, out, input_1, safety)
+    pmb.test_computation()
+
+
+def main():
+    test() 
+
+
+if __name__ == "__main__":
+    main()
