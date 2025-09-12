e0fad4c6fff9dc0e0e5c4d56e8e59fa21b2000cd
Zabro000 <reinzabrocki@shaw.ca>
2025-09-11 20:34:47 -0600

    Addded function to run calculations for the 12V buck converter

--- Component and Board Calculations/NEW LM76005 BUCK CALCULATIONS.py
+++ Component and Board Calculations/NEW LM76005 BUCK CALCULATIONS.py
@@ -18,8 +18,9 @@ def buck_converter_5V_calcs():
     eff = 0.9
     turn_on = 14.5
     undershoot = 100e-3 
+    my_capacitor = 180e-6
     
-    buck_1 = LM76005(fsw, vin, vout, current, eff, tss, ripple_ratio, ripple_ratios, fb_rtop, en_rtop, pg_rtop, turn_on, undershoot)
+    buck_1 = LM76005(fsw, vin, vout, current, eff, tss, ripple_ratio, ripple_ratios, fb_rtop, en_rtop, pg_rtop, turn_on, undershoot, my_capacitor)
 
     print(f"\n\nINPUT VOLTAGE IS {buck_1.input_voltage}V\n\n")
     buck_1.block_standard_run_calculations()
@@ -31,9 +32,34 @@ def buck_converter_5V_calcs():
 
     print(5 * 0.021)
 
+def buck_converter_12V_calcs():
+    fsw = 400e3
+    vin =  48
+    vout = 12
+    tss = 30e-3
+    ripple_ratio = 0.2
+    ripple_ratios = [ripple_ratio, ripple_ratio * 2]
+    fb_rtop = 10e3
+    en_rtop = 100e3
+    pg_rtop = 100e3
+    current = 5
+    eff = 0.9
+    turn_on = 14.5
+    undershoot = 100e-3 
+    my_capacitor = 180e-6
+    
+    buck_1 = LM76005(fsw, vin, vout, current, eff, tss, ripple_ratio, ripple_ratios, fb_rtop, en_rtop, pg_rtop, turn_on, undershoot, my_capacitor)
+
+    print(f"\n\nINPUT VOLTAGE IS {buck_1.input_voltage}V\n\n")
+    buck_1.block_standard_run_calculations()
+
+    buck_1.input_voltage = 3.7 * 5
+
+    print(f"\n\nINPUT VOLTAGE IS {buck_1.input_voltage}V\n\n")
+    buck_1.block_standard_run_calculations()
 
 def main():
-    buck_converter_5V_calcs()
+    buck_converter_12V_calcs()
 
 
 if __name__ == "__main__":
