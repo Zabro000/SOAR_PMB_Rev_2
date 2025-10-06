fe93b6aec937e16f05a1c3f70e4f1e89c9a8e8f2
Zabro000 <reinzabrocki@shaw.ca>
2025-10-06 00:26:27 -0600

    Different values for the ideal diodes

--- Component and Board Calculations/LM73100RPWR calcs.py
+++ Component and Board Calculations/LM73100RPWR calcs.py
@@ -24,20 +24,20 @@ def module_1_3V3():
     diode_1.current_monitor_resistor_calcualtions(True)
 
 def module_1_5V():
-    diode_1 = L(None, None, None, 6, 4, 5.5, 100000, 1.0e4, 4.8)
+    diode_1 = L(None, None, None, 6, 4, 5.5, 100000, 3e4, 4.8)
     diode_1.uv_ov_resistor_divider_calculations(True)
     diode_1.power_good_resistor_divider_calculations(True)
     diode_1.current_monitor_resistor_calcualtions(True)
 
 
 def module_1_12V():
-    diode_1 = L(None, None, None, 14, 10, 5.5, 100000, 1.0e4, 11.5)
+    diode_1 = L(None, None, None, 15, 11, 5.5, 100000, 3e4, 11.8)
     diode_1.uv_ov_resistor_divider_calculations(True)
     diode_1.power_good_resistor_divider_calculations(True)
     diode_1.current_monitor_resistor_calcualtions(True)
 
 def main():
-    module_1_5V()
+    module_1_12V()
 
 if __name__ == "__main__":
     main()
